b6131e7d53df1ab1d12cb4d39317d6ad317ec462
Ewan <tech-a06@Tech-A06s-MacBook-Air.local>
2018-02-21 11:09:57 +0000

    Added mongoose

--- index.js
+++ index.js
@@ -4,44 +4,51 @@ const ejs = require("ejs");
 const fs = require("fs");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+app.set("view engine", "ejs");
 const port = 3000;
+
 
 function startServer() {
 	app.get("/", (req, res) => {
 		console.log("Request recieved for " + req.url);
-		res.render("index", {
-			tubeData: tubeData,
-			dlrData: dlrData,
-			routes: routes
-		});
+		res.render("index");
 		console.log("Sent " + req.url);
 	});
-	app.get("/routes", (req, res) => {
-		console.log("Request recieved for " + req.url + " from " + req.connection.remoteAddress);
-		res.render("routes", {
-			tubeData: tubeData,
-			dlrData: dlrData,
-			routes: routes
-		});
-		console.log("Sent " + req.url);
-	});
-	app.get("/index.css", (req, res) => {
+	app.get("/global.css", (req, res) => {
 		console.log("Request recieved for " + req.url + " from " + req.connection.remoteAddress);
 		res.writeHead(200, {'Content-Type': 'text/css'});
-		res.write(fs.readFileSync("./index.css","utf-8"));
+		res.write(fs.readFileSync("./resources/global.css","utf-8"));
 		res.end();
 		console.log("Sent " + req.url);
 	});
 	app.get("/script.js", (req,res) => {
 		console.log("Request recieved for " + req.url + " from " + req.connection.remoteAddress);
 		res.writeHead(200, {'Content-Type': 'text/javascript'});
-		res.write(fs.readFileSync("./script.js","utf-8"));
+		res.write(fs.readFileSync("./resources/script.js","utf-8"));
  		res.end();
  		console.log("Sent " + req.url);
+	});
+	app.post("/addlab", (req, res) => {
+
 	});
 	app.listen(port, () => {
 		console.log("Server started on port " + port); 
 	});
 }
 
+function connectToDb() {
+	const connection = mongoose.connect("mongodb://34.243.248.231:27017");
+	const resultModel = mongoose.model('Result', new Schema({
+		studentName: String,
+		teacherName: String,
+		score: String,
+		labName: String,
+		time: String,
+		date: Date
+	}))
+}
+
+connectToDb();
 startServer()
